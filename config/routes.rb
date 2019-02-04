db17176e67698dfd632f2f1b56227d6a21e7d008
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-04 11:54:42 +0000

    react gem added and installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-    namespace :api do
-      namespace :v1 do
-        resources :items, only: [:index, :create, :destroy, :update]
-      end
+  root to: 'site#index'
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: [:index, :create, :destroy, :update]
     end
   end
+end
